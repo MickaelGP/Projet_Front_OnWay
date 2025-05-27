4a7e0d46a20b9475980528b1a1be4660951ec815
unknown <mgperret@icloud.com>
2025-05-27 20:48:18 +0200

    Fin recherche et details

---
+++ src/app/api/connexion/route.ts
@@ -0,0 +1,25 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+import { NextResponse } from "next/server";
+import { API_ROUTES, API_URL } from "@/lib/apiRoutes";
+
+export async function POST(request: Request) {
+    const { utilEmail, utilMdp } = await request.json();
+
+    const apiExterne = await fetch(API_URL + API_ROUTES.connexion, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ utilEmail, utilMdp }),
+    });
+
+    const data = await apiExterne.json();
+
+    const setCookie = apiExterne.headers.get("set-cookie");
+    const response = NextResponse.json({ data }, { status: apiExterne.status });
+    if (setCookie) {
+      response.headers.set("set-cookie", setCookie);
+    }
+    return response;
+}
--- src/ui/Form/FormConnexion.tsx
+++ src/ui/Form/FormConnexion.tsx
@@ -5,10 +5,10 @@ export default function FormConnexion() {
             <div className="container w-75 py-5">
                 <form className="my-5">
                     <div className="mb-3">
-                        <input type="email" className="form-control" name="UtilEmail" id="connexionEmail" placeholder="Email :" required />
+                        <input type="email" className="form-control" name="utilEmail" id="connexionEmail" placeholder="Email :" required />
                     </div>
                     <div className="mb-3">
-                        <input type="password" className="form-control" name="UtilMdp" id="connexionPassword" placeholder="Mot de passe :" required/>
+                        <input type="password" className="form-control" name="utilMdp" id="connexionPassword" placeholder="Mot de passe :" required/>
                     </div>
                     <div className="text-center">
                         <button type="submit" className="btn btn-primary" id="btnConexion">Connexion</button>
